88263f3af34ff9c53e613ead9ca3aa2b205516ea
Harvey Yemm <hjyemm@icloud.com>
2024-12-28 23:05:02 +0000

    Removed unused vars

--- app/components/layout/NavItems.tsx
+++ app/components/layout/NavItems.tsx
@@ -12,7 +12,6 @@ import {
 import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Products from '../components/Sections/Products'
+
 const page = () => {
   return (
     <div className='w-[70vw] m-auto'>
       <div className='w-full grid grid-cols-2 gap-40 my-10'>
-        <div className='rounded-md shado-sm'>
-          <img src='/forklift.jpg' className='rounded-xl brightness-50 max-w-[400px min-h-[400px] object-cover shadow-sm ' alt="Stock" />
+        <div className='rounded-md shadow-sm'>
+          <img src='/forklift.jpg' className='rounded-xl brightness-50 max-w-[400px] min-h-[400px] object-cover shadow-sm' alt="Stock" />
         </div>
-        <div className='w-full h-full text-left content-center  m-auto'>
+        <div className='w-full h-full text-left content-center m-auto'>
           <p>At Ryzenz, we stock a range of products to meet all your needs. From the latest tech gadgets to everyday essentials, we have it all. Browse our collection and find what you're looking for today!</p>
         </div>
       </div>
       <div className='py-6'>
-      <Products/>
+        <Products />
       </div>
     </div>
   )
 }
 
-export default page
+export default page;
