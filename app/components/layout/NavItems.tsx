e6a42bf742117e72ddb1279aa1a20fa606a1ab96
Harvey Yemm <hjyemm@icloud.com>
2024-12-28 23:00:34 +0000

    Removed unused vars

--- app/components/layout/NavItems.tsx
+++ app/components/layout/NavItems.tsx
@@ -3,9 +3,6 @@
 import * as React from "react"
 import Link from "next/link"
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Popover,
   PopoverContent,
@@ -19,48 +16,11 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-import { ChevronDown, ChevronRight, ChevronUp } from "lucide-react"
+import { ChevronRight } from "lucide-react"
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
+
 
 export function NavItems() {
     return (
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Products from '../components/Sections/Products'
-
 const page = () => {
   return (
     <div className='w-[70vw] m-auto'>
       <div className='w-full grid grid-cols-2 gap-40 my-10'>
         <div className='rounded-md shado-sm'>
-          <img src={Forklift.src} className='rounded-xl brightness-50 max-w-[400px min-h-[400px] object-cover shadow-sm ' alt="Stock" />
+          <img src='/forklift.jpg' className='rounded-xl brightness-50 max-w-[400px min-h-[400px] object-cover shadow-sm ' alt="Stock" />
         </div>
         <div className='w-full h-full text-left content-center  m-auto'>
           <p>At Ryzenz, we stock a range of products to meet all your needs. From the latest tech gadgets to everyday essentials, we have it all. Browse our collection and find what you're looking for today!</p>
